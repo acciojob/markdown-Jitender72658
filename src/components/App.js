3ce569e4763ed3a4a2106f275ad2cb4d5fe44eff
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-10-25 19:09:26 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useState} from "react";
 
+const App = ()=>{
+    const[content, setContent] = useState("");
+
+    function modifyPreview(e){
+         const newContent = e.target.value;
+         setContent(newContent);
+    }
+    return ( 
+        <div className="app">
+           <div className="preview"><textarea onChange={modifyPreview} placeholder="Hello" >{content}</textarea></div>
+           <div className="loading"><textarea  onChange={modifyPreview} value={content}></textarea></div>
+        </div>
+    )
+}
+
+export default App;
